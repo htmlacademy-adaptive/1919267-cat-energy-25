16e526dd8bb3e4c8e32b4a14a56b9f9d5df25d54
Keks <keks@htmlacademy.ru>
2022-04-30 22:17:09 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/script.js
+++ 7/js/script.js
@@ -1,10 +1,27 @@
-let burger = document.querySelector('.burger');
-let navigation = document.querySelector('.navigation');
+const burger = document.querySelector('.burger');
+const navigation = document.querySelector('.navigation');
+const rangeButtonBefore = document.querySelector('.range-button-before');
+const rangeButtonAfter = document.querySelector('.range-button-after');
+const sliderScreenBefore = document.querySelector('.slider__screen--before');
+const rangeBar = document.querySelector('.range__bar');
 
 navigation.classList.remove('main-header__navigation--nojs');
 
-burger.addEventListener('click', function() {
+burger.addEventListener('click', function(evt) {
+  evt.preventDefault();
   burger.classList.toggle('burger--opened');
   navigation.classList.toggle('main-header__navigation--opened');
   navigation.classList.toggle('main-header__navigation--closed');
-})
+});
+
+rangeButtonBefore.addEventListener('click', function(evt) {
+  evt.preventDefault();
+  sliderScreenBefore.classList.remove('slider__screen--zero');
+  rangeBar.classList.remove('range__bar--after');
+});
+
+rangeButtonAfter.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  sliderScreenBefore.classList.add('slider__screen--zero');
+  rangeBar.classList.add('range__bar--after');
+});
